e27ba3c6678ec5bf32e66b44c7346e085ec96a73
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-07-14 23:13:27 -0700

    feat: wrapping ffi related code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod cli;
 mod client;
 mod clients;
 mod config;
+mod ffi_wrapper;
 mod handlers;
 mod line;
 mod listener;
@@ -93,7 +94,7 @@ fn main() -> Result<(), anyhow::Error> {
                         "ACCEPT host={} port={} fd={} n={}/{}",
                         client.ipaddr,
                         client.port,
-                        client.fd.as_raw_fd(),
+                        client.tcp_stream.as_raw_fd(),
                         clients.len(),
                         config.max_clients
                     );
