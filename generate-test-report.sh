a967b76d0648c9348938d08b5eb85f6eb78e8183
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-02-15 22:06:45 -0700

    fix: added comments

--- generate-test-report.sh
+++ generate-test-report.sh
@@ -4,12 +4,15 @@ BASE_RUSTFLAGS=""
 CARGO_FEATURES=""
 
 export RUSTFLAGS="${BASE_RUSTFLAGS} --allow=warnings -Cinstrument-coverage"
+
 # build-* ones are not parsed by grcov
 export LLVM_PROFILE_FILE="profiling/build-%p-%m.profraw"
-
 cargo build ${CARGO_FEATURES} --all-targets --locked --workspace --verbose
+
+# cleanup old values
 find -name '*.profraw' | xargs rm
-# different
+
+# different from the `cargo build` ones
 LLVM_PROFILE_FILE="profiling/profile-%p-%m.profraw"
 cargo nextest run --profile ci --no-fail-fast ${CARGO_FEATURES} --all-targets --workspace
 
