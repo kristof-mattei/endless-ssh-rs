b9e5c3cf40a3abca5eee0d36ee1c0eeb74041ab6
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2026-02-15 21:36:21 -0700

    fix: spacing

--- generate-test-report.sh
+++ generate-test-report.sh
@@ -14,15 +14,15 @@ LLVM_PROFILE_FILE="profiling/profile-%p-%m.profraw"
 cargo nextest run --profile ci --no-fail-fast ${CARGO_FEATURES} --all-targets --workspace
 
 grcov $(find . -name "profile-*.profraw" -print) \
-  --binary-path ./target/debug/ \
-  --branch \
-  --excl-br-line "^\s*((debug_)?assert(_eq|_ne)?!)" \
-  --excl-br-start "mod tests \{" \
-  --excl-line "(#\\[derive\\()|(^\s*.await[;,]?$)" \
-  --excl-start "mod tests \{" \
-  --ignore-not-existing \
-  --keep-only "crates/**" \
-  --llvm \
-  --output-path ./reports/ \
-  --output-type lcov,html,markdown \
-  --source-dir .
+    --binary-path ./target/debug/ \
+    --branch \
+    --excl-br-line "^\s*((debug_)?assert(_eq|_ne)?!)" \
+    --excl-br-start "mod tests \{" \
+    --excl-line "(#\\[derive\\()|(^\s*.await[;,]?$)" \
+    --excl-start "mod tests \{" \
+    --ignore-not-existing \
+    --keep-only "crates/**" \
+    --llvm \
+    --output-path ./reports/ \
+    --output-type lcov,html,markdown \
+    --source-dir .
